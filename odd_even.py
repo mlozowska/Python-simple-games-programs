9738beaf3e5b426916df3149be7c89ecbd1b0aab
Magdalena Lozowska <60215258+mlozowska@users.noreply.github.com>
2021-01-13 19:43:04 +0100

    Odd or even?

---
+++ odd_even.py
@@ -0,0 +1,15 @@
+#ODD OR EVERN?
+# practice: Modulo operator
+
+print('Welcome to the ODD or EVEN game!')
+number = int(input('Enter the number :'))
+
+if (number % 2 == 0):
+    print('You\'ve selected an EVEN number!')
+elif (number % 2 != 0):
+    print('You\'ve selected an ODD number!')
+
+
+
+
+
