c3217b2b887694b1dbe8d0eb30dbc5d22cc8a2a5
Magdalena Lozowska <60215258+mlozowska@users.noreply.github.com>
2021-02-06 11:43:19 +0100

    web scraping - weather forecast
    Create 10-Day Weather for Warsaw, Masovian Voivodeship, Poland for weather.com website

--- weather_forecast.py
+++ weather_forecast.py
@@ -1,22 +1,22 @@
+# 10-Day Weather for Warsaw, Masovian Voivodeship, Poland 
 from bs4 import BeautifulSoup
 import requests
 
-
 url = 'https://weather.com/en-IN/weather/tenday/l/a8b0daa43d13b260354967e7b6792eae37e9924dee035d82922e416e63de4051'
 response = requests.get(url)
-# print(response)
 data = response.text
-# print(data)
 soup = BeautifulSoup(data, 'html.parser')
 
-weathers = soup.find_all('summary', {'class': 'Disclosure--Summary--AvowU DaypartDetails--Summary--2nJx1 Disclosure--hideBorderOnSummaryOpen--LEvZQ'})
+weathers = soup.find_all('summary', {
+    'class': 'Disclosure--Summary--AvowU DaypartDetails--Summary--2nJx1 Disclosure--hideBorderOnSummaryOpen--LEvZQ'})
 
 for weather in weathers:
-    days = soup.find('h2', {'class': 'DetailsSummary--daypartName--1Mebr'}).text
-    # degrees = soup.find('span',{'data-testid':'TemperatureValue'}).text
-    # types = soup.find('span',{'class':'DetailsSummary--extendedData--aaFeV'}).text
-    # night_temp = soup.find('span',{'data-testid':'PercentageValue'}).text
-    # print('Days: ', days, 'Temperature:', degrees, 'Type of weather:', types, 'Temperature at night:', night_temp)
-    print(days)
+    dates = weather.find('h2', {'class': 'DetailsSummary--daypartName--1Mebr'}).text
+    day_temp = weather.find('span', {'class': 'DetailsSummary--highTempValue--3x6cL'}).text
+    night_temp = weather.find('span', {'class': 'DetailsSummary--lowTempValue--1DlJK'}).text
+    types = weather.find('span',{'class':'DetailsSummary--extendedData--aaFeV'}).text
+    humidity = weather.find('span', {'data-testid': 'PercentageValue'}).text
+    wind = weather.find('span', {'class': 'Wind--windWrapper--1Va1P undefined'}).text
 
-
+    print('\nDay:', dates, '\nDay Temperature:', day_temp, '\nNight Temperature:', night_temp, '\nHumidity:', humidity,
+          '\nWind:', wind, '\nType of weather:', types)
